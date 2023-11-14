dc6ec48612ad1468c99f6451435052c529c28ef0
charcochoco <127725153+charcochoco@users.noreply.github.com>
2023-11-14 13:57:05 +0100

    Add files via upload

---
+++ src/graphql/resolvers.js
@@ -0,0 +1,39 @@
+const { Post } = require("../models/Post.js");
+const { Comment } = require("../models/Comment.js");
+
+const resolvers = {
+  Query: {
+    posts: async (parent, args) => {
+      const {order} = args;
+      return await Post.find().sort({createdAt: order});
+    },
+    post: async (parent, args) => {
+        const {id} = args;
+        return await Post.findById(id);
+    },
+    commentsByPostId: async (parent, args) => {
+      const {postId} = args;
+      return await Comment.find({ postId: postId });
+    },
+  },
+  Mutation: {
+    createPost: async (parent, args) => {
+      const { author, title, link } = args;
+      const post = new Post({ author, title, link });
+      await post.save();
+      return post;
+    },
+    deletePost: async (parent, args) => {
+      const { id } = args;
+      return await Post.findByIdAndDelete(id);
+    },
+    createComment: async (parent, args) => {
+      const { author, content, postId } = args;
+      const comment = new Comment({ author, content, postId });
+      await comment.save();
+      return comment;
+    },
+  },
+};
+
+module.exports = { resolvers };
